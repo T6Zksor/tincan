b811250fcb9faee2ed581a1c31391abae10a04d9
t6zksor <t6zksor@gmail.com>
2022-06-01 00:14:13 +0800

    asteroid instancing

--- src/main.cc
+++ src/main.cc
@@ -89,16 +89,16 @@ int main()
 	//Model ourModel("../resources/objects/backpack/backpack.obj");
     //Model nanosuit("../resources/objects/nanosuit/nanosuit.obj"); 
 	Model rock("../resources/objects/rock/rock.obj");
-	Model planet("../resources/objects/planet/planet.obj");
+	//Model planet("../resources/objects/planet/planet.obj");
 
 	// generate a large list of semi-random model transformation matrices
 	// ------------------------------------------------------------------
-	unsigned int amount = 1000;
+	unsigned int amount = 100000;
 	glm::mat4* modelMatrices;
 	modelMatrices = new glm::mat4[amount];
 	srand(static_cast<unsigned int>(glfwGetTime())); // initialize random seed
-	float radius = 50.0;
-	float offset = 2.5f;
+	float radius = 150.0;
+	float offset = 25.0f;
 	for (unsigned int i = 0; i < amount; i++)
 	{
 		glm::mat4 model = glm::mat4(1.0f);
@@ -124,6 +124,34 @@ int main()
 		modelMatrices[i] = model;
 	}
 
+	// vertex buffer object
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, amount * sizeof(glm::mat4), &modelMatrices[0], GL_STATIC_DRAW);
+
+	for (unsigned int i = 0; i < rock.meshes.size(); i++)
+	{
+		unsigned int VAO = rock.meshes[i].VAO;
+		glBindVertexArray(VAO);
+		// vertex attributes
+		std::size_t vec4Size = sizeof(glm::vec4);
+		glEnableVertexAttribArray(3);
+		glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)0);
+		glEnableVertexAttribArray(4);
+		glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)(1 * vec4Size));
+		glEnableVertexAttribArray(5);
+		glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)(2 * vec4Size));
+		glEnableVertexAttribArray(6);
+		glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, 4 * vec4Size, (void*)(3 * vec4Size));
+
+		glVertexAttribDivisor(3, 1);
+		glVertexAttribDivisor(4, 1);
+		glVertexAttribDivisor(5, 1);
+		glVertexAttribDivisor(6, 1);
+
+		glBindVertexArray(0);
+	}
 
 	// load textures
 	// -------------
@@ -166,24 +194,24 @@ int main()
 
 		// configure transformation matrices
 		glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 1000.0f);
-		glm::mat4 view = camera.GetViewMatrix();;
+		glm::mat4 view = camera.GetViewMatrix();
 		shader.use();
 		shader.setMat4("projection", projection);
 		shader.setMat4("view", view);
 
-		// draw planet
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
-		shader.setMat4("model", model);
-		planet.Draw(shader);
-
 		// draw meteorites
-		for (unsigned int i = 0; i < amount; i++)
+		shader.use();
+		shader.setInt("texture_diffuse1", 0);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, rock.textures_loaded[0].id); // note: we also made the textures_loaded vector public (instead of private) from the model class.
+		for (unsigned int i = 0; i < rock.meshes.size(); i++)
 		{
-			shader.setMat4("model", modelMatrices[i]);
-			rock.Draw(shader);
+			glBindVertexArray(rock.meshes[i].VAO);
+			glDrawElementsInstanced(GL_TRIANGLES, static_cast<unsigned int>(rock.meshes[i].indices.size()), GL_UNSIGNED_INT, 0, amount);
+			glBindVertexArray(0);
 		}
+
+
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
